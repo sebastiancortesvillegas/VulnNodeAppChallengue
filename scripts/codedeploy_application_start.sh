6396fca8cab85b2510dcb3006c8468e5d1751dcf
JuanGit <sebastiancortesvillegas@gmail.com>
2024-05-11 22:14:51 -0500

    Add fail git

--- scripts/codedeploy_application_start.sh
+++ scripts/codedeploy_application_start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 
 #ECR Login
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 960917724930.dkr.ecr.us-east-1.amazonaws.com
 
 #Pulling image from ECR
-docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CONTAINER:latest
+docker pull 960917724930.dkr.ecr.us-east-1.amazonaws.com/vulnnodeapp:latest
 
 #Creating and starting a docker container using a new image
-docker run -p 80:80 --rm $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CONTAINER:latest
+docker run -p 80:80 --rm 960917724930.dkr.ecr.us-east-1.amazonaws.com/vulnnodeapp:latest
+
