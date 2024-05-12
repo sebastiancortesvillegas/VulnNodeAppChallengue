abb79a9384bae09a2bc3c2eb616d6ee270755c3b
JuanGit <sebastiancortesvillegas@gmail.com>
2024-05-11 22:35:01 -0500

    Add check two

--- scripts/codedeploy_application_start.sh
+++ scripts/codedeploy_application_start.sh
@@ -7,5 +7,5 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker pull 960917724930.dkr.ecr.us-east-1.amazonaws.com/vulnnodeapp:latest
 
 #Creating and starting a docker container using a new image
-docker run -p 80:80 --rm 960917724930.dkr.ecr.us-east-1.amazonaws.com/vulnnodeapp:latest
+docker run -p 3000:3000 --rm 960917724930.dkr.ecr.us-east-1.amazonaws.com/vulnnodeapp:latest
 
