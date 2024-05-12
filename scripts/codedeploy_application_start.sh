53760ab57c03422e3edd135b6482f7e714fe9569
JuanGit <sebastiancortesvillegas@gmail.com>
2024-05-11 20:51:25 -0500

    Add docker script

--- scripts/codedeploy_application_start.sh
+++ scripts/codedeploy_application_start.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
-cd /home/ubuntu/Nodeapp-vulnode
-pm2 delete server
-pm2 start app.js
+#!/bin/bash 
 
+#ECR Login
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+
+#Pulling image from ECR
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CONTAINER:latest
+
+#Creating and starting a docker container using a new image
+docker run -p 80:80 --rm $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CONTAINER:latest
