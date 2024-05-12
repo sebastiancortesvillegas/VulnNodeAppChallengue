000efc818038371df9fea8d47960d3ce9c74afae
JuanGit <sebastiancortesvillegas@gmail.com>
2024-05-12 17:33:30 -0500

    Add dockerrun

--- scripts/codedeploy_application_start.sh
+++ scripts/codedeploy_application_start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash 
 
-#ECR Login
+# ECR Login
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 960917724930.dkr.ecr.us-east-1.amazonaws.com
 
-#Pulling image from ECR
+# Eliminar la imagen anterior si existe
+docker image rm -f 960917724930.dkr.ecr.us-east-1.amazonaws.com/vulnnodeapp:latest
+
+# Pulling image from ECR
 docker pull 960917724930.dkr.ecr.us-east-1.amazonaws.com/vulnnodeapp:latest
 
-#Creating and starting a docker container using a new image
-docker run -p 3000:3000 --rm 960917724930.dkr.ecr.us-east-1.amazonaws.com/vulnnodeapp:latest
-
+# Creating and starting a docker container using a new image
+docker run -d -p 3000:3000 960917724930.dkr.ecr.us-east-1.amazonaws.com/vulnnodeapp:latest
